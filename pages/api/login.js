a6d45aa1a7e24d2fbab1f74aad386dce7c8d2758
Chris Kavanagh <ckava3@gmail.com>
2022-03-08 22:22:25 -0500

    login, logout

---
+++ pages/api/login.js
@@ -0,0 +1,18 @@
+import cookie from "cookie";
+
+const login = (req, res) => {
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("token", req.body.token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 60 * 60,
+      sameSite: "strict",
+      path: "/",
+    })
+  );
+  res.statusCode = 200;
+  res.json({ success: true });
+};
+
+export default login;
---
+++ pages/api/logout.js
@@ -0,0 +1,18 @@
+import cookie from "cookie";
+
+const logout = (req, res) => {
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      expires: new Date(0),
+      sameSite: "strict",
+      path: "/",
+    })
+  );
+  res.statusCode = 200;
+  res.json({ success: true });
+};
+
+export default logout;
